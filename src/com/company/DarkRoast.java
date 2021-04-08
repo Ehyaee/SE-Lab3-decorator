fdc14258bfc5770f69a5fad29c41b272fd9d98cf
narges <jatages@gmail.com>
2021-04-08 13:53:42 +0430

    Implement DarkRoast func to pass test

--- src/com/company/DarkRoast.java
+++ src/com/company/DarkRoast.java
@@ -3,11 +3,11 @@ package com.company;
 public class DarkRoast implements Beverage {
     @Override
     public String getDescription() {
-        return null;
+        return "Delicious DarkRoast";
     }
 
     @Override
     public double cost() {
-        return 0;
+        return 0.7;
     }
 }
