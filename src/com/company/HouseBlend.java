30bc70273bcf77631bb87a5e7186f65b22f7f552
Ehyaee <z.ehyaee96@gmail.com>
2021-04-08 12:40:00 +0430

    Solve test compile error

--- src/com/company/HouseBlend.java
+++ src/com/company/HouseBlend.java
@@ -1,4 +1,14 @@
 package com.company;
 
-public class HouseBlend {
+public class HouseBlend implements Beverage {
+
+    @Override
+    public int cost() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
 }
