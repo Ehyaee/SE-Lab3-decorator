2988f9dd732465a597370b459058b1e01cc2539a
Ehyaee <z.ehyaee96@gmail.com>
2021-04-08 14:44:29 +0430

    Solve test compile time error

--- src/com/company/SteamedMilk.java
+++ src/com/company/SteamedMilk.java
@@ -1,4 +1,17 @@
 package com.company;
 
-public class SteamedMilk {
+public class SteamedMilk extends CondimentDecorator{
+    public SteamedMilk(Beverage beverage){
+        super(beverage);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
+
+    @Override
+    public double cost() {
+        return super.cost();
+    }
 }
