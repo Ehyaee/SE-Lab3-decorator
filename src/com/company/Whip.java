e476be59547c26fe5a6de46c469547c53c31771e
Ehyaee <z.ehyaee96@gmail.com>
2021-04-08 15:10:20 +0430

    Complete implementation to pass test

--- src/com/company/Whip.java
+++ src/com/company/Whip.java
@@ -7,12 +7,20 @@ public class Whip extends CondimentDecorator {
 
     @Override
     public String getDescription() {
-        return super.getDescription();
+        return super.getDescription() + " with " + getCondimentName();
+    }
+
+    private String getCondimentName(){
+        return "whip";
     }
 
     @Override
     public double cost() {
-        return super.cost();
+        return super.cost() + getCondimentCost();
+    }
+
+    private double getCondimentCost(){
+        return 0.10;
     }
 }
 
