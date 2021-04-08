bbb25bd3e4af9f0b1e70a1527972c72e9f3b35ae
Ehyaee <z.ehyaee96@gmail.com>
2021-04-08 15:08:37 +0430

    Solve test compile time error

--- src/com/company/Whip.java
+++ src/com/company/Whip.java
@@ -1,4 +1,18 @@
 package com.company;
 
-public class Whip {
+public class Whip extends CondimentDecorator {
+    public Whip(Beverage beverage){
+        super(beverage);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
+
+    @Override
+    public double cost() {
+        return super.cost();
+    }
 }
+
