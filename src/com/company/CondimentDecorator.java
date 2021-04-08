fa59481d7172f3b82e11d079d75883ae66de49c8
Ehyaee <z.ehyaee96@gmail.com>
2021-04-08 15:01:12 +0430

    Solve test compile time error

--- src/com/company/CondimentDecorator.java
+++ src/com/company/CondimentDecorator.java
@@ -1,4 +1,24 @@
 package com.company;
 
-public class CondimentDecorator {
-}
+
+    public class CondimentDecorator implements Beverage {
+        private Beverage _beverage;
+
+        CondimentDecorator(Beverage beverage){
+            this._beverage = beverage;
+        }
+
+        private Beverage getBeverage(){
+            return _beverage;
+        }
+
+        @Override
+        public String getDescription() {
+            return getBeverage().getDescription();
+        }
+
+        @Override
+        public double cost() {
+            return getBeverage().cost();
+        }
+    }
--- src/com/company/Mocha.java
+++ src/com/company/Mocha.java
@@ -1,4 +1,16 @@
 package com.company;
 
-public class Mocha {
+public class Mocha extends CondimentDecorator {
+    public Mocha(Beverage beverage){
+        super(beverage);
+    }
+    @Override
+    public double cost() {
+        return super.cost();
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
 }
